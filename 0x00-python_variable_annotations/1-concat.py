063bfa00a50486064df33587d041588cbcc26d58
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-04-18 17:24:36 +0000

    create: function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Function to concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return a string
+    """
+    return str1 + str2
