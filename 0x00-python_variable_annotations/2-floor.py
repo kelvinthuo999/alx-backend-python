15e695f76ce218a452699c7c07bbefdf23ce967f
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-04-18 17:37:02 +0000

    create: function to round a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+"""
+Function to round off a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function to round off a float
+    """
+    return math.floor(n)
