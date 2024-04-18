366d9421e25415aad08578f3bd91946ab0856b88
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-04-18 20:27:45 +0000

    create: function to sum floats in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Function to sum floats in a list"""
+
+
+from typing import List, Union
+
+
+def sum_list(input_list: List[float]) -> Union[float, int]:
+    """Function to sum floats in a list"""
+    return sum(input_list)
