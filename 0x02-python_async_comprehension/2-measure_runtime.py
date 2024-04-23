ba0f58eb74b5353b76da84390f0a2ddd01de606b
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-04-23 13:10:55 +0000

    fix: use asyncio time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """Function to measure time"""
-import time
 import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def async_comprehension() -> float:
-    start_time = time.time()
-    job = [async_comprehension for item in range(4)]
-    await gather(*job)
-    end_time = time.time()
-    return (end_time - start_time)
+async def measure_runtime() -> float:
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = asyncio.get_event_loop().time()
+    return end_time - start_time
