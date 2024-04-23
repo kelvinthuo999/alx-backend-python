295476636b298143ec173b62624e30b266b0529b
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-04-23 09:33:42 +0000

    create: function to create random numbers after 1 sec

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Function to run coroutine 10 times"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Function to generate random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
